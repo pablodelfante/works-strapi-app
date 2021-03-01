f69ddc2f5d8358a944bfa16e05cc8c656466983b
pablo <pablodelfantexp@gmail.com>
2021-03-01 17:41:46 -0300

    borre cosas innecesarias en plugin sobre cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -5,22 +5,7 @@ module.exports = ({ env }) => ({
       providerOptions: {
         cloud_name: env('CLOUDINARY_NAME', 'lapabla'),
         api_key: env('CLOUDINARY_KEY', 625372782318994),
-        api_secret: env('CLOUDINARY_SECRET', BHFI5cWUKSmr1tA9DJIw9opAu5Y),
-      },
-      //midleware parser tamaño maximo de imagenes
-      parser: {
-        enabled: true,
-        multipart: true,
-        formidable: {
-          maxFileSize: 2000 * 2000 // Defaults to 200mb
-        }
-      },
-      breakpoints: {
-        xlarge: 0,
-        large: 0,
-        medium: 0,
-        small: 0,
-        xsmall: 0
+        api_secret: env('CLOUDINARY_SECRET', 'BHFI5cWUKSmr1tA9DJIw9opAu5Y'),
       }
       //---------
     },
